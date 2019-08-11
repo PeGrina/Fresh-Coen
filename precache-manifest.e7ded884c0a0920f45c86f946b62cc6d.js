cf8e877015fc11eccc3d00ec4a5504a2779d5518
PeGrina <a.pavlov123123@gmail.com>
2019-08-11 21:14:11 +0500

    Updates

---
+++ precache-manifest.e7ded884c0a0920f45c86f946b62cc6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6ee12f468f94e51ebf8",
+    "url": "/static/js/main.9d3358e8.chunk.js"
+  },
+  {
+    "revision": "21100ecaef3fbe3900ae",
+    "url": "/static/js/2.02904898.chunk.js"
+  },
+  {
+    "revision": "e6ee12f468f94e51ebf8",
+    "url": "/static/css/main.7caaa281.chunk.css"
+  },
+  {
+    "revision": "21100ecaef3fbe3900ae",
+    "url": "/static/css/2.ce55e8fd.chunk.css"
+  },
+  {
+    "revision": "8e8b76da10b468fc1c7b89511061d6e7",
+    "url": "/index.html"
+  }
+];
